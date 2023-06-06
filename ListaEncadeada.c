4d46806f780975691e7c2134906bbbf855f4882e
Jeiel Lucas <98417296+JeielLucas@users.noreply.github.com>
2023-06-05 22:43:29 -0300

    Add files via upload
    Lista encadeada feita na linguagem C

---
+++ ListaEncadeada.c
@@ -0,0 +1,70 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct no{
+    char* nome;
+    int numero;
+    struct no *prox;
+}Jogador;
+
+
+Jogador *inicio = NULL;
+int tam = 0;
+
+void adicionarJogador(char* nome, int numero){
+    Jogador * novo=malloc(sizeof(Jogador));
+    novo->nome = nome;
+    novo->numero = numero;
+    novo->prox = NULL;
+
+    if(inicio == NULL){
+        inicio = novo;
+        tam++;
+    }
+    else{
+        Jogador *aux = inicio;
+        while(aux->prox != NULL){
+            aux = aux->prox;
+        }
+        aux->prox = novo;
+        tam++;
+    }
+}
+
+void listarJogador(Jogador *aux){
+    printf("O nome do jogador e: %s e o numero dele e: %i\n", aux->nome, aux->numero);
+    if(aux->prox != NULL){
+        aux = aux->prox;
+        listarJogador(aux);
+    }
+}
+
+void removerJogador(char* jogador){
+    Jogador* lixo = NULL;
+    if(inicio->nome == jogador){
+        lixo = inicio;
+        inicio = inicio->prox;
+        free(lixo);
+    }
+    else{
+        Jogador* aux = inicio ->prox;
+        while(aux->prox != NULL){
+            if(aux->prox->nome == jogador){
+                Jogador* atual = aux ->prox;
+                aux->prox = aux->prox->prox;
+                free(atual);
+            }else{
+                aux = aux->prox;
+            }
+        }
+    }
+}
+
+int main(){
+    adicionarJogador("Neymar", 10);
+    adicionarJogador("Mbappe", 7);
+    adicionarJogador("Sergio Ramos", 4);
+    adicionarJogador("Hakimi", 2);
+    removerJogador("Neymar");
+    listarJogador(inicio);
+}
